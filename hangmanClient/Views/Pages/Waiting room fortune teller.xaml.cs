0946d8af392220ad5891d256ca57f9f33ff3771e
danimsix <dani.morales.sixto@gmail.com>
2024-04-20 19:03:01 -0600

    new pages

---
+++ hangmanClient/Views/Pages/Waiting room fortune teller.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Views.Pages
+{
+    /// <summary>
+    /// Lógica de interacción para Waiting_room_fortune_teller.xaml
+    /// </summary>
+    public partial class Waiting_room_fortune_teller : Page
+    {
+        public Waiting_room_fortune_teller()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- hangmanClient/Views/obj/Debug/Views_Content.g.i.cs
+++ hangmanClient/Views/obj/Debug/Views_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/arrow.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/rabbit.png")]
 
 
